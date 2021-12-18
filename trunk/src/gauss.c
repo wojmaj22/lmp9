5849c85ca04b79384d6882800ef523e35bfeb784
wojmaj22 <wojjmaj22@gmail.com>
2021-12-18 21:16:32 +0100

    Zmiany

--- trunk/src/gauss.c
+++ trunk/src/gauss.c
@@ -6,27 +6,8 @@
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
 int eliminate(Matrix *matrix, Matrix *b){
-	int wiersze = matrix -> r;
-	int kolumny = matrix -> c;
-	double **A = matrix->data;
 
-	for( int k=0; k < kolumny-1; k++)
-	{
-		for( int w=k+1; w < wiersze; w++)
-		{
-			printf("Zmiana eletentu [%i][%i]\n", w, k);
-			double wsp = A[w][k] / A[0][k];
-			printf("WSP = %lf", wsp);
-			for( int i=0; i< kolumny; i++)
-			{
-				A[w][i] -= wsp*A[0][i];
-			}
-			b->data[w][0] -= wsp*b->data[0][0];
-		}
-	}
 
-	printToScreen(matrix);
-	printf("Koniec Gaussa\n");
 
 return 0;
 }
